eb7c65a64493df134f1c29933a183b39f5e3117a
geraldvillaran <gerald.villaran.dev@gmail.com>
2024-12-15 20:07:56 +0800

    remove google maps key in GoogleMaps.tsx

--- src/app/main/documentation/material-ui-components/components/autocomplete/GoogleMaps.tsx
+++ src/app/main/documentation/material-ui-components/components/autocomplete/GoogleMaps.tsx
@@ -10,7 +10,7 @@ import { debounce } from '@mui/material/utils';
 
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
-const GOOGLE_MAPS_API_KEY = 'AIzaSyC3aviU6KHXAjoSnxcw6qbOhjnFctbxPkE';
+const GOOGLE_MAPS_API_KEY = 'your-google-maps-api-key';
 
 function loadScript(src: string, position: HTMLElement | null, id: string) {
   if (!position) {
